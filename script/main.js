6613daea2886993f4ac58f0f081b638cb3cef8db
s.sky <87020882+skysuthar@users.noreply.github.com>
2025-03-08 20:17:34 +0530

    update photosh

--- script/main.js
+++ script/main.js
@@ -1,31 +1,28 @@
 // Import the data to customize and insert them into page
-const fetchData = () => {
-  // fetch("customize.json")
-  //   .then(data => data.json())
-  //   .then(data => {
-  //     dataArr = Object.keys(data);
-  //     dataArr.map(customData => {
-  //       if (data[customData] !== "") {
-  //         if (customData === "imagePath") {
-  //           document
-  //             .querySelector(`[data-node-name*="${customData}"]`)
-  //             .setAttribute("src", data[customData]);
-  //         } else {
-  //           document.querySelector(`[data-node-name*="${customData}"]`).innerText = data[customData];
-  //         }
-  //       }
 
-  //       // Check if the iteration is over
-  //       // Run amimation if so
-  //       if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
+window.addEventListener('load', () => {
+  Swal.fire({
+      title: 'Do you want to play music in the background?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+  }).then((result) => {
+      if (result.isConfirmed) {
+          document.querySelector('.song').play();
           animationTimeline();
-    //     } 
-    //   });
-    // });
-};
+      } else {
+          animationTimeline();
+      }
+  });
+});
 
 // Animation Timeline
 const animationTimeline = () => {
+  // document.querySelector('.song').play();
+
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
@@ -301,6 +298,3 @@ const animationTimeline = () => {
     tl.restart();
   });
 };
-
-// Run fetch and animation in sequence
-fetchData();
